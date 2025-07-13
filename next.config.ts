08d1a6188fb137b587a1278e8990ff667a06bb49
Adamu Adamu Manu <balaaadhamu45@gmail.com>
2025-07-13 18:23:14 +0100

    chore: disable type and lint errors for build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
-const frontendUrl = process.env.NEXT_PUBLIC_URL;
-const domain = frontendUrl ? new URL(frontendUrl).hostname : "localhost";
-
 const nextConfig: NextConfig = {
   images: {
-    domains: [domain], // Only needed if loading remote images
+    unoptimized: true, // ✅ disables Next.js Image Optimization
   },
   eslint: {
     ignoreDuringBuilds: true,
